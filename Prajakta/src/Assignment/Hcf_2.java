329de18e4d22b56bf7d6c48e20074a20159f3efe
User <User@DESKTOP-JD1BI73>
2022-10-22 18:33:33 +0530

    My commit

---
+++ Prajakta/src/Assignment/Hcf_2.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class Hcf_2 {
+
+	public static void main(String[] args) {
+		 int num1=36;
+		  int num2=60; 
+		  int hcf=0;
+
+		    for (int i=1;i<=num1 || i<=num2;i++)
+		      {
+		     if (num1%i==0 && num2%i==0)
+		        hcf=i;
+		      }
+
+		    System.out.println("HCF: "+ hcf);
+		  }
+
+}
---
+++ Prajakta/src/Assignment/String_asc_order.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+public class String_asc_order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
